79614a9c3221669e9d6839b982b6ad9a4d25bccd
Paulo Curado <phcurado@gmail.com>
2024-07-12 16:18:35 +0300

    add keymaps for navigation

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -4,5 +4,23 @@ local opts = {
   silent = true, -- do not show message
 }
 
+
+-- Move up and down with JK
 vim.keymap.set('n', '<C-j>', '<C-e>', opts)
 vim.keymap.set('n', '<C-k>', '<C-y>', opts)
+
+-- Move selected lines in visual mode up or down, awesome!
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- split windows
+vim.keymap.set("n", "<leader>sv", ":vs<CR>")
+vim.keymap.set("n", "<leader>sh", ":split<CR>")
+
+-- start a terminal with iex
+vim.keymap.set("n", "<leader>ex", ":below 18 sp<CR>:term<CR>iiex", { silent = true })
+
+
+-- Go to middle and center
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
