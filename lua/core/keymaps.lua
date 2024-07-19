fb911b0951c4e3e5d4711984d115deb2a230165f
Paulo Curado <phcurado@gmail.com>
2024-07-19 16:46:03 +0300

    formatter

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,13 +1,12 @@
 -- define common options
 local opts = {
-  noremap = true, -- non-recursive
-  silent = true, -- do not show message
+	noremap = true, -- non-recursive
+	silent = true, -- do not show message
 }
 
-
 -- Move up and down with JK
-vim.keymap.set('n', '<C-j>', '<C-e>', opts)
-vim.keymap.set('n', '<C-k>', '<C-y>', opts)
+vim.keymap.set("n", "<C-j>", "<C-e>", opts)
+vim.keymap.set("n", "<C-k>", "<C-y>", opts)
 
 -- Move selected lines in visual mode up or down, awesome!
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -19,7 +18,6 @@ vim.keymap.set("n", "<leader>sh", ":split<CR>")
 
 -- start a terminal with iex
 vim.keymap.set("n", "<leader>ex", ":below 18 sp<CR>:term<CR>iiex", { silent = true })
-
 
 -- Go to middle and center
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
