ab9b0a46f613c8ce56701c58aaeb8f66568e9a45
Paulo Curado <phcurado@gmail.com>
2024-07-20 16:04:48 +0300

    add more themes with ondedark as default

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,10 +1,26 @@
 return {
-  {
-    'Mofiqul/dracula.nvim',
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.cmd([[colorscheme dracula]])
-    end,
-  },
+	{
+		"Mofiqul/dracula.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			-- vim.cmd([[colorscheme dracula]])
+		end,
+	},
+	{
+		"navarasu/onedark.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.cmd([[colorscheme onedark]])
+		end,
+	},
+	{
+		"ellisonleao/gruvbox.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			-- vim.cmd([[colorscheme gruvbox]])
+		end,
+	},
 }
