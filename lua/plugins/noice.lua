00b15816a320244584b8f97b5be10141717b1d2a
Paulo Curado <phcurado@gmail.com>
2024-08-18 18:22:42 +0300

    add noice and upd dependencies

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,15 @@
+return {
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		opts = {
+			cmdline = {
+				view = "cmdline", -- view for rendering the cmdline. Change to `cmdline` to get a classic cmdline at the bottom
+			},
+		},
+		dependencies = {
+			"MunifTanjim/nui.nvim",
+			"rcarriga/nvim-notify",
+		},
+	},
+}
