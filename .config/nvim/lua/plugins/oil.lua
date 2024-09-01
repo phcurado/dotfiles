d43016e377eb4deb71b0c042bad262115ed4b173
Paulo Curado <phcurado@gmail.com>
2024-09-01 11:12:37 +0300

    add stow ignore and show hidden on Oil plugin

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -5,7 +5,12 @@ return {
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 		config = function()
 			vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-			require("oil").setup()
+			require("oil").setup({
+				view_options = {
+					-- Show files and directories that start with "."
+					show_hidden = true,
+				},
+			})
 		end,
 	},
 }
