2f83a2364202a21629970d4f61df45a20f2d6561
Paulo Curado <phcurado@gmail.com>
2025-01-22 13:34:58 +0200

    change persistence session plugin

--- .config/nvim/lua/core/autocmds.lua
+++ .config/nvim/lua/core/autocmds.lua
@@ -24,30 +24,6 @@ vim.api.nvim_create_autocmd("FileType", {
 	end,
 })
 
-vim.api.nvim_create_autocmd("VimEnter", {
-	callback = function()
-		-- Only load the session if nvim was started with no args and without reading from stdin
-		if vim.fn.argc(-1) == 0 and not vim.g.using_stdin then
-			-- Save these to a different directory, so our manual sessions don't get polluted
-			require("resession").load(vim.fn.getcwd(), { dir = "dirsession", silence_errors = true })
-		end
-	end,
-	nested = true,
-})
-
-vim.api.nvim_create_autocmd("VimLeavePre", {
-	callback = function()
-		require("resession").save(vim.fn.getcwd(), { dir = "dirsession", notify = false })
-	end,
-})
-
-vim.api.nvim_create_autocmd("StdinReadPre", {
-	callback = function()
-		-- Store this for later
-		vim.g.using_stdin = true
-	end,
-})
-
 -- LSP
 vim.api.nvim_create_autocmd("LspAttach", {
 	group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
---
+++ .config/nvim/lua/plugins/persistence.lua
@@ -0,0 +1,10 @@
+return {
+	-- Persist the session based on git branch
+	{
+		"folke/persistence.nvim",
+		event = "BufReadPre", -- this will only start session saving when an actual file was opened
+		opts = {
+			-- add any custom options here
+		},
+	},
+}
