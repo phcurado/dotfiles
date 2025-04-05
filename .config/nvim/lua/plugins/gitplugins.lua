6aa5eb615aaf2d237189b5bfab47e61a27d3f16d
Paulo Curado <phcurado@gmail.com>
2025-04-05 13:35:01 +0300

    upd plugins

--- .config/nvim/lua/plugins/gitplugins.lua
+++ .config/nvim/lua/plugins/gitplugins.lua
@@ -1,22 +1,26 @@
 return {
-	{
-		"lewis6991/gitsigns.nvim",
-		config = function()
-			require("gitsigns").setup({
-				current_line_blame = true,
-			})
-		end,
-	},
-	{
-		"NeogitOrg/neogit",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"sindrets/diffview.nvim",
-			"nvim-telescope/telescope.nvim",
-		},
-		keys = {
-			{ "<leader>gg", ":lua require('neogit').open()<CR>", desc = "Open Neogit" },
-		},
-		config = true,
-	},
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup({
+        current_line_blame = true,
+      })
+    end,
+  },
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "sindrets/diffview.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+    keys = {
+      { "<leader>gg", ":lua require('neogit').open()<CR>", desc = "Open Neogit" },
+    },
+    config = function()
+      require("neogit").setup({
+        disable_signs = true,
+      })
+    end,
+  },
 }
