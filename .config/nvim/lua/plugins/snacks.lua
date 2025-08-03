9b983336307581aa135d8ee7f2e3b2ef16e25445
Paulo Curado <phcurado@gmail.com>
2025-08-03 10:08:27 +0300

    remove autopairs

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -91,7 +91,7 @@ return {
           finder = "files",
           format = "file",
           show_empty = true,
-          hidden = false,
+          hidden = true,
           ignored = false,
           follow = false,
           supports_live = true,
