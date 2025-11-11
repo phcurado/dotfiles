061289b7a29752dff6f496cfc1c852fb8dd752c9
Paulo Curado <phcurado@gmail.com>
2025-11-11 09:58:21 +0200

    upd deps

--- .config/nvim/lua/plugins/fyler.lua
+++ .config/nvim/lua/plugins/fyler.lua
@@ -1,9 +1,10 @@
 return {
   "A7Lavinraj/fyler.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  opts = { icon_provider = "nvim_web_devicons" },
-  config = function(_, opts)
-    local fyler = require("fyler").setup(opts)
-    vim.keymap.set("n", "<leader>n", ":Fyler kind=split_left<CR>", { desc = "Toggle File Explorer" })
+  dependencies = { "nvim-mini/mini.icons" },
+  branch = "stable",
+  opts = {},
+  config = function()
+    require("fyler").setup()
+    vim.keymap.set("n", "<leader>n", ":Fyler kind=split_left_most<CR>", { desc = "Toggle File Explorer" })
   end,
 }
