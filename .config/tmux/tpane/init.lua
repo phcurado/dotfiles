528b03f36b92d0584ef37acca0f5be8968343691
Paulo Curado <phcurado@gmail.com>
2026-06-26 22:29:20 +0300

    tpane theme support

--- .config/tmux/tpane/init.lua
+++ .config/tmux/tpane/init.lua
@@ -1,6 +1,8 @@
+tpane.use("themes")
 tpane.use("vim-navigator")
 tpane.use("yank")
 
+tpane.theme("Catppuccin Mocha", { transparent = true })
 
 tpane.on("window:close", function(window)
 	tpane.tmux.cleanup(window)
