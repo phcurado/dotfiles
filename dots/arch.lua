e29bf3d30313baddf56300fd6b65f07185c16999
Paulo Curado <phcurado@gmail.com>
2026-06-29 22:48:35 +0300

    add all packages

--- dots/arch.lua
+++ dots/arch.lua
@@ -1,11 +1,73 @@
+local common_packages = require("dots.packages")
+
+local packages = {
+	"1password",
+	"1password-cli",
+	"bluetui",
+	"brave-bin",
+	"cava",
+	"cliphist",
+	"ddcutil",
+	"discord",
+	"docker",
+	"docker-compose",
+	"ghostty",
+	"github-cli",
+	"grim",
+	"gpu-screen-recorder",
+	"inotify-tools",
+	"jujutsu",
+	"nautilus",
+	"neovim-nightly-bin",
+	"networkmanager",
+	"niri",
+	"noctalia-git",
+	"obsidian",
+	"polkit-kde-agent",
+	"postgresql",
+	"power-profiles-daemon",
+	"qt5-wayland",
+	"qt6-wayland",
+	"sddm",
+	"slurp",
+	"tailscale",
+	"ttf-0xproto-nerd",
+	"unzip",
+	"wl-clipboard",
+	"wlsunset",
+	"wtype",
+	"xdg-desktop-portal-gnome",
+	"xdg-desktop-portal-gtk",
+	"xwayland-satellite",
+}
+
+local groups = {
+	"docker",
+	"wheel",
+}
+
+local systemd_services = {
+	"bluetooth.service",
+	"docker.service",
+	"NetworkManager.service",
+	"postgresql.service",
+	"tailscaled.service",
+}
+
 dots.symlink("~/.config/niri", ".config/niri")
 dots.symlink("~/.config/noctalia", ".config/noctalia")
 dots.symlink("~/.config/wallpapers", ".config/wallpapers")
 
-dots.pacman.install({ "base-devel", "git", "paru" })
-dots.paru.install({ "1password", "bat", "docker", "ripgrep" })
+dots.pacman.install({
+	"base-devel",
+	"git",
+	"paru",
+})
 
-dots.user.groups({ "docker" })
+dots.paru.install(common_packages)
+dots.paru.install(packages)
 
-dots.systemd.enable({ "docker.service" })
-dots.systemd.start({ "docker.service" })
+dots.user.groups(groups)
+
+dots.systemd.enable(systemd_services)
+dots.systemd.start(systemd_services)
--- dots/macos.lua
+++ dots/macos.lua
@@ -1,9 +1,51 @@
+local common_packages = require("dots.packages")
+
+local packages = {
+	"fzf",
+	"git",
+	"gh",
+	"jj",
+	"neovim",
+	"sqlite",
+	"sketchybar",
+	"borders",
+	"lua",
+	"blueutil",
+}
+
+local casks = {
+	"nikitabobko/tap/aerospace",
+	"ghostty",
+	"brave-browser",
+	"discord",
+	"obsidian",
+	"docker-desktop",
+	"tailscale-app",
+	"1password",
+	"1password-cli",
+	"font-0xproto-nerd-font",
+	"font-sf-pro",
+	"font-sf-mono",
+	"sf-symbols",
+	"font-sketchybar-app-font",
+}
+
+local services = {
+	"sketchybar",
+	"borders",
+}
+
 dots.symlink("~/.config/aerospace", ".config/aerospace")
 dots.symlink("~/.config/borders", ".config/borders")
 dots.symlink("~/.config/sketchybar", ".config/sketchybar")
 
-dots.brew.tap({ "FelixKratz/formulae" })
-dots.brew.install({ "bat", "ripgrep", "sketchybar", "borders", "blueutil" })
-dots.brew.cask({ "ghostty", "obsidian" })
+dots.brew.tap({
+	"FelixKratz/formulae",
+})
 
-dots.brew.service.start({ "sketchybar", "borders" })
+dots.brew.install(common_packages)
+dots.brew.install(packages)
+
+dots.brew.cask(casks)
+
+dots.brew.service.start(services)
