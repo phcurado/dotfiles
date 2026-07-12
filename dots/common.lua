cbd191028a50ae2361e4283aba33d77e76065717
Paulo Curado <phcurado@gmail.com>
2026-07-12 12:33:35 +0100

    ssh managed by dots

--- dots/common.lua
+++ dots/common.lua
@@ -1,5 +1,18 @@
 dots.user.shell("zsh")
 
+local ssh_key = dots.ssh.keypair("default", {
+	path = "~/.ssh/id_ed25519",
+	passphrase = false,
+})
+
+dots.output("ssh_public_key", {
+	value = ssh_key.public_key,
+})
+
+dots.output("ssh_fingerprint", {
+	value = ssh_key.fingerprint,
+})
+
 dots.docker.compose("searxng", {
 	file = ".config/searxng/docker-compose.yml",
 })
