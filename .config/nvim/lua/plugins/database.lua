09f7111d484f51daf12ddb36c98d452ee2acdcb9
Paulo Curado <phcurado@gmail.com>
2026-07-20 19:44:27 +0100

    add dbee, replacing dbui on nvim

--- .config/nvim/lua/plugins/database.lua
+++ .config/nvim/lua/plugins/database.lua
@@ -1,21 +1,14 @@
 return {
-  "kristijanhusak/vim-dadbod-ui",
-  dependencies = {
-    { "tpope/vim-dadbod", lazy = true },
-    { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true }, -- Optional
+  {
+    "kndndrj/nvim-dbee",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+    },
+    build = function()
+      require("dbee").install()
+    end,
+    config = function()
+      require("dbee").setup()
+    end,
   },
-  cmd = {
-    "DBUI",
-    "DBUIToggle",
-    "DBUIAddConnection",
-    "DBUIFindBuffer",
-  },
-  init = function()
-    -- Your DBUI configuration
-    vim.g.db_ui_use_nerd_fonts = 1
-
-    vim.g.dbs = {
-      { name = "dev", url = "postgres://postgres:postgres@localhost:5432/" },
-    }
-  end,
 }
